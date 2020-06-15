e0b56705b676c9f278cc7e22ad0e6686d8d23280
Alessandro Canicatti <ales.canicatti@gmail.com>
2020-06-15 13:53:51 +0200

    stub views addition

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello world! This is the polls index page.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting at question %s." % question_id)
